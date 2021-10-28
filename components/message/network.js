b83775e9d318f2cb1cb4728af607dd528bf2c57f
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-28 18:18:14 -0500

    Almacenando informacion

---
+++ components/message/network.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const response = require('../../network/response');
+const controller = require('./controller');
+
+router.get('/', function (req, res) {
+    controller.getMessages().then((messageList)=>{
+        response.success(req, res, messageList, 200);
+    })
+    .catch((error)=>{
+        response.error(req, res, "Invalid data", 500, "Server Error")
+    })
+});
+router.post('/', function (req, res) {
+    const body = req.body;
+    controller.addMessage(body.user, body.message).then((fullMessage)=>{
+        console.log('[success Network]', fullMessage);
+        response.success(req, res, fullMessage, 201);
+    }).catch(error=>{
+        console.error('[error Network]', error);
+        response.error(req, res, "Informacion invalida", 400, "Error en los campos agregados.");
+    });
+});
+router.delete('/', function (req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    response.success(req, res, 'Mensaje eliminado', 200)
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,41 +2,15 @@ const express = require('express');
 const PORT = 3000;
 const response = require('./network/response')
 // Router
-const router = express.Router();
+// const router = require('./components/message/network');
+const router = require('./network/routes');
 // Inicializar
 var app = express();
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(router);
+// app.use(router);
 
-router.get('/', function(req, res) {
-    res.send('hola desde get'); 
-})
-router.get('/message', function (req, res) {
-    console.log('headers',req.headers)
-    res.header({
-        "custom-header": "Nuestro valor personalizado",
-    })
-    // res.send('Lista de mensajes');
-    response.success(req, res, "Lista de mensajes");
-});
-router.post('/message', function (req, res) {
-    console.log("body",req.body)
-    console.log('query', req.query);
-    if(req.query.error == 'ok'){
-        response.error(req, res, "Error Ocurrido", 500, "es una simulación de los errores.");
-    }
-    else{
-        response.success(req, res, 'Mensaje '+req.body.message+' añadido', 201);
-    }
-});
-router.delete('/message', function (req, res) {
-    console.log(req.query);
-    console.log(req.body);
-    // res.status(201).send({error:'', body:"Eliminado"});
-    response.success(req, res, 'Mensaje eliminado', 200)
-
-})
+router(app)
 // app.use('/', function(req, res) {
 //     res.send('Hola');
 // });
