345b215298fbfdc5385cdd53b2cc16804b10f923
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-28 12:08:13 -0500

    respuesta coherente y static files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const PORT = 3000;
+const response = require('./network/response')
 // Router
 const router = express.Router();
 // Inicializar
 var app = express();
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(router);
 
 router.get('/', function(req, res) {
@@ -16,22 +17,29 @@ router.get('/message', function (req, res) {
     res.header({
         "custom-header": "Nuestro valor personalizado",
     })
-    res.send('Lista de mensajes');
+    // res.send('Lista de mensajes');
+    response.success(req, res, "Lista de mensajes");
 });
 router.post('/message', function (req, res) {
     console.log("body",req.body)
     console.log('query', req.query);
-    res.send('Mensaje '+req.body.message+' añadido');
+    if(req.query.error == 'ok'){
+        response.error(req, res, "Error Ocurrido", 401)
+    }
+    // res.send('Mensaje '+req.body.message+' añadido');
+    response.success(req, res, 'Mensaje '+req.body.message+' añadido', 201)
 });
 router.delete('/message', function (req, res) {
     console.log(req.query);
     console.log(req.body);
-    res.status(201).send({error:'', body:"Eliminado"});
+    // res.status(201).send({error:'', body:"Eliminado"});
+    response.success(req, res, 'Mensaje eliminado', 200)
+
 })
 // app.use('/', function(req, res) {
 //     res.send('Hola');
 // });
-
+app.use('/app', express.static('public'))
 app.listen(PORT);
 
 console.log(`La aplicación está escuchando en http://localhost:${PORT}`)
